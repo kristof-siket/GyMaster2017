e2c0c4a78c28c99ffbf65d355403d1317a159f04
kristof-siket <kristofsiket@gmail.com>
2017-04-25 19:17:08 +0200

    ViewModel megírva.

---
+++ GyMaster/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic;
+
+namespace GyMaster
+{
+    /// <summary>
+    /// Singleton tervezésű ViewModel az MVVM pattern implementálásához.
+    /// </summary>
+    public class ViewModel
+    {
+        Logic bl;
+
+        public Logic BL
+        {
+            get { return bl; }
+            private set { bl = value; }
+        }
+
+        private static ViewModel _peldany;
+
+        /// <summary>
+        /// Visszaadja az egyetlen ViewModel példányt, ha nem létezik, létrehozza.
+        /// </summary>
+        /// <returns>A ViewModel.</returns>
+        public static ViewModel Get()
+        {
+            if (_peldany == null)
+                _peldany = new ViewModel();
+            return _peldany;
+        }
+
+        private ViewModel()
+        {
+            bl = new Logic();
+        }
+    }
+}
